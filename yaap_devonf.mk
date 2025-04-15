df66cdc9b86c59acb11fbd124453d82780dbd4af
Adarsh Singh <singh.adarsh@aol.com>
2025-04-15 12:30:12 +0530

    devonf: Adapt to YAAP

--- yaap_devonf.mk
+++ yaap_devonf.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/devonf/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_devonf
+PRODUCT_NAME := yaap_devonf
 PRODUCT_DEVICE := devonf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
